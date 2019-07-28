7dccf662aa771111e139182b7eca78118604fcd1
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-28 17:20:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,8 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
+        
         },
       :matriarch => {
         #your key/value pairs here
