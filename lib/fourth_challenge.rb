37e5aaff8b609b553f1ad552c1c73dd73f805423
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-28 17:29:53 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        friend1 = {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        }
+        friend2 = {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
       ]
    }, 
    :capulet => {
